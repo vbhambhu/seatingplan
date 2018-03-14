679a82fb8fa5297b5ad276a0d8d883e32de2e6bc
vbhambhu <v.bhambhu083@gmail.com>
2018-03-14 20:31:27 +0000

    Adding users management

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -11,3 +11,9 @@ $.fn.exists = function(callback) {
 $('.datatable').exists(function() {
     $(".datatable").DataTable();
 });
+
+$('#sidebarCollapse').on('click', function () {
+    $('#sidebar, #content').toggleClass('active');
+    //$('.collapse.in').toggleClass('in');
+    //$('a[aria-expanded=true]').attr('aria-expanded', 'false');
+});
